b2fe583478b1f69313ac0628e141d43805bb9d95
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-04-28 00:10:14 +0800

    Add NanaGet project.

---
+++ NanaGet/App.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+
+#include "App.h"
+#include "App.g.cpp"
+#include "MainPage.h"
+
+using namespace winrt;
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::Foundation;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace NanaGet;
+using namespace NanaGet::implementation;
+
+namespace winrt::NanaGet::implementation
+{
+    App::App()
+    {
+        Initialize();
+
+        AddRef();
+        m_inner.as<::IUnknown>()->Release();
+    }
+
+    App::~App()
+    {
+        Close();
+    }
+}
