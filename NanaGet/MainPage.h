dc3c8737193e7b4002f74c9da3ed2a33dff694e9
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-04-29 23:51:03 +0800

    Add the UI layout of list view.

--- NanaGet/MainPage.h
+++ NanaGet/MainPage.h
@@ -7,11 +7,6 @@ namespace winrt::NanaGet::implementation
     struct MainPage : MainPageT<MainPage>
     {
         MainPage();
-
-        int32_t MyProperty();
-        void MyProperty(int32_t value);
-
-        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
     };
 }
 
