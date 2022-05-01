357234c85304dc29ce7d94cd23bd838cbaf24cf5
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-05-01 20:12:32 +0800

    Add new task, settings and about page.

--- NanaGet/MainPage.cpp
+++ NanaGet/MainPage.cpp
@@ -2,6 +2,9 @@
 #include "MainPage.h"
 #include "MainPage.g.cpp"
 
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.System.h>
+
 using namespace winrt;
 using namespace Windows::UI::Xaml;
 
@@ -10,5 +13,114 @@ namespace winrt::NanaGet::implementation
     MainPage::MainPage()
     {
         InitializeComponent();
+    }
+
+    void MainPage::NewTaskButtonClick(
+        IInspectable const& sender,
+        RoutedEventArgs const& e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        using winrt::Windows::UI::Xaml::Visibility;
+
+        this->NewTaskGrid().Visibility(Visibility::Visible);
+    }
+
+    void MainPage::SettingsButtonClick(
+        IInspectable const& sender,
+        RoutedEventArgs const& e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        using winrt::Windows::UI::Xaml::Visibility;
+
+        this->SettingsGrid().Visibility(Visibility::Visible);
+    }
+
+    void MainPage::AboutButtonClick(
+        IInspectable const& sender,
+        RoutedEventArgs const& e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        using winrt::Windows::UI::Xaml::Visibility;
+
+        this->AboutGrid().Visibility(Visibility::Visible);
+    }
+
+    void  MainPage::NewTaskGridDownloadButtonClick(
+        IInspectable const& sender,
+        RoutedEventArgs const& e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        using winrt::Windows::UI::Xaml::Visibility;
+
+        this->NewTaskGrid().Visibility(Visibility::Collapsed);
+    }
+
+    void  MainPage::NewTaskGridCancelButtonClick(
+        IInspectable const& sender,
+        RoutedEventArgs const& e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        using winrt::Windows::UI::Xaml::Visibility;
+
+        this->NewTaskGrid().Visibility(Visibility::Collapsed);
+    }
+
+    void MainPage::SettingsGridApplyButtonClick(
+        IInspectable const& sender,
+        RoutedEventArgs const& e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        using winrt::Windows::UI::Xaml::Visibility;
+
+        this->SettingsGrid().Visibility(Visibility::Collapsed);
+    }
+
+    void MainPage::SettingsGridCancelButtonClick(
+        IInspectable const& sender,
+        RoutedEventArgs const& e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        using winrt::Windows::UI::Xaml::Visibility;
+
+        this->SettingsGrid().Visibility(Visibility::Collapsed);
+    }
+
+    void MainPage::AboutGridGitHubButtonClick(
+        IInspectable const& sender,
+        RoutedEventArgs const& e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        using winrt::Windows::Foundation::Uri;
+        using winrt::Windows::System::Launcher;
+
+        Launcher::LaunchUriAsync(Uri(L"https://github.com/M2Team/NanaGet"));
+    }
+
+    void MainPage::AboutGridCancelButtonClick(
+        IInspectable const& sender,
+        RoutedEventArgs const& e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        using winrt::Windows::UI::Xaml::Visibility;
+
+        this->AboutGrid().Visibility(Visibility::Collapsed);
     }
 }
--- NanaGet/MainPage.h
+++ NanaGet/MainPage.h
@@ -4,9 +4,48 @@
 
 namespace winrt::NanaGet::implementation
 {
+    using Windows::Foundation::IInspectable;
+    using Windows::UI::Xaml::RoutedEventArgs;
+
     struct MainPage : MainPageT<MainPage>
     {
         MainPage();
+
+        void NewTaskButtonClick(
+            IInspectable const& sender,
+            RoutedEventArgs const& e);
+
+        void SettingsButtonClick(
+            IInspectable const& sender,
+            RoutedEventArgs const& e);
+
+        void AboutButtonClick(
+            IInspectable const& sender,
+            RoutedEventArgs const& e);
+
+        void NewTaskGridDownloadButtonClick(
+            IInspectable const& sender,
+            RoutedEventArgs const& e);
+
+        void NewTaskGridCancelButtonClick(
+            IInspectable const& sender,
+            RoutedEventArgs const& e);
+
+        void SettingsGridApplyButtonClick(
+            IInspectable const& sender,
+            RoutedEventArgs const& e);
+
+        void SettingsGridCancelButtonClick(
+            IInspectable const& sender,
+            RoutedEventArgs const& e);
+
+        void AboutGridGitHubButtonClick(
+            IInspectable const& sender,
+            RoutedEventArgs const& e);
+
+        void AboutGridCancelButtonClick(
+            IInspectable const& sender,
+            RoutedEventArgs const& e);
     };
 }
 
