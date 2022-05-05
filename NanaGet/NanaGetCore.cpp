f9af1a843b8a11a4a0cab18515e8e9c4fd9989f2
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-05-05 13:48:18 +0800

    Add NanaGetCore.

---
+++ NanaGet/NanaGetCore.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   NanaGet
+ * FILE:      NanaGetCore.cpp
+ * PURPOSE:   Implementation for NanaGet Core Implementation
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include "NanaGetCore.h"
---
+++ NanaGet/NanaGetCore.h
@@ -0,0 +1,22 @@
+﻿/*
+ * PROJECT:   NanaGet
+ * FILE:      NanaGetCore.h
+ * PURPOSE:   Definition for NanaGet Core Implementation
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#pragma once
+
+#ifndef NANA_GET_CORE
+#define NANA_GET_CORE
+
+#if (defined(__cplusplus) && __cplusplus >= 201703L)
+#elif (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L)
+#else
+#error "[Mile] You should use a C++ compiler with the C++17 standard."
+#endif
+
+#endif // !NANA_GET_CORE
