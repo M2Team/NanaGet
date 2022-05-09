59fd8457479044e4de31bc77566aa3643ca333fc
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-05-09 20:24:11 +0800

    Update Mile.Project.ModernWindows.

--- NanaGet/NanaGetCore.cpp
+++ NanaGet/NanaGetCore.cpp
@@ -10,10 +10,16 @@
 
 #include "NanaGetCore.h"
 
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
 #include <Windows.h>
 #include <objbase.h>
 
+#include <WinSock2.h>
 #include <WS2tcpip.h>
+#include <iphlpapi.h>
 #pragma comment(lib, "ws2_32.lib")
 
 #include <winrt/Windows.Storage.h>
