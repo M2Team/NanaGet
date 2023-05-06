253a4a37a9f6e5a1770f2cf6eb80fdd182c10150
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-05-06 22:49:28 +0800

    Add WinRT XAML UnhandledException handler for debugging.

--- NanaGet/App.cpp
+++ NanaGet/App.cpp
@@ -9,6 +9,19 @@ namespace winrt::NanaGet::implementation
     App::App()
     {
         ::MileXamlGlobalInitialize();
+
+        using Windows::Foundation::IInspectable;
+        using Windows::UI::Xaml::UnhandledExceptionEventArgs;
+
+        this->UnhandledException([](
+            IInspectable const& sender,
+            UnhandledExceptionEventArgs const& e)
+        {
+            UNREFERENCED_PARAMETER(sender);
+
+            e.Handled(true);
+            throw winrt::hresult_error(e.Exception(), e.Message());       
+        });
     }
 
     void App::Close()
