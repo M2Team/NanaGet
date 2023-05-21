67d5a6c7f08ab5f95fa199aca920de3d7cf13920
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-05-22 03:43:53 +0800

    Show error dialog when Windows Runtime XAML meeting the unhandlded exception.

--- NanaGet/App.cpp
+++ NanaGet/App.cpp
@@ -4,6 +4,16 @@
 
 #include <Mile.Xaml.h>
 
+#include <CommCtrl.h>
+#pragma comment(lib,"comctl32.lib")
+
+#include "NanaGetCore.h"
+
+namespace NanaGet
+{
+    extern HWND MainWindowHandle;
+}
+
 namespace winrt::NanaGet::implementation
 {
     App::App()
@@ -18,6 +28,16 @@ namespace winrt::NanaGet::implementation
             UnhandledExceptionEventArgs const& e)
         {
             UNREFERENCED_PARAMETER(sender);
+
+            ::TaskDialog(
+                NanaGet::MainWindowHandle,
+                nullptr,
+                L"NanaGet",
+                e.Message().c_str(),
+                nullptr,
+                TDCBF_OK_BUTTON,
+                TD_ERROR_ICON,
+                nullptr);
 
             e.Handled(true);       
         });
