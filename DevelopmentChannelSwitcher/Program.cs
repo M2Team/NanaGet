0df586aea5252d8da0a29aeb2a496a60f94fa083
MouriNaruto <KurikoMouri@outlook.jp>
2023-07-16 03:39:46 +0800

    Update DevelopmentChannelSwitcher implementation.

--- DevelopmentChannelSwitcher/Program.cs
+++ DevelopmentChannelSwitcher/Program.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.VisualBasic;
-using Mile.Project.Helpers;
+﻿using Mile.Project.Helpers;
 using System.Text;
 
 namespace DevelopmentChannelSwitcher
@@ -11,36 +10,79 @@ namespace DevelopmentChannelSwitcher
             string ProjectRootPath = GitRepository.GetRootPath();
             Console.WriteLine(ProjectRootPath);
 
-            List<(string Release, string Preview)> ReplacementStringList =
-                new List<(string Release, string Preview)> ();
-            ReplacementStringList.Add((
-                "DisplayName=\"NanaGet\"",
-                "DisplayName=\"NanaGet Preview\""));
-            ReplacementStringList.Add((
-                "Name=\"40174MouriNaruto.NanaGet\"",
-                "Name=\"40174MouriNaruto.NanaGetPreview\""));
-            ReplacementStringList.Add((
-                "<DisplayName>NanaGet</DisplayName>",
-                "<DisplayName>NanaGet Preview</DisplayName>"));
+            bool Preview = true;
 
-            string FilePath = string.Format(
-                @"{0}\NanaGetPackage\Package.appxmanifest",
-                ProjectRootPath);
-
-            string Content = File.ReadAllText(
-                FilePath,
-                Encoding.UTF8);
-            foreach (var ReplacementStringItem in ReplacementStringList)
             {
-                Content = Content.Replace(
-                    ReplacementStringItem.Release,
-                    ReplacementStringItem.Preview);
-                //Content = Content.Replace(
-                //    ReplacementStringItem.Preview,
-                //    ReplacementStringItem.Release);
+                List<(string Release, string Preview)> ReplacementStringList =
+                new List<(string Release, string Preview)>();
+                ReplacementStringList.Add((
+                    "DisplayName=\"NanaGet\"",
+                    "DisplayName=\"NanaGet Preview\""));
+                ReplacementStringList.Add((
+                    "Name=\"40174MouriNaruto.NanaGet\"",
+                    "Name=\"40174MouriNaruto.NanaGetPreview\""));
+                ReplacementStringList.Add((
+                    "<DisplayName>NanaGet</DisplayName>",
+                    "<DisplayName>NanaGet Preview</DisplayName>"));
+
+                string FilePath = string.Format(
+                    @"{0}\NanaGetPackage\Package.appxmanifest",
+                    ProjectRootPath);
+
+                string Content = File.ReadAllText(
+                    FilePath,
+                    Encoding.UTF8);
+                foreach (var ReplacementStringItem in ReplacementStringList)
+                {
+                    if (Preview)
+                    {
+                        Content = Content.Replace(
+                            ReplacementStringItem.Release,
+                            ReplacementStringItem.Preview);
+                    }
+                    else
+                    {
+                        Content = Content.Replace(
+                            ReplacementStringItem.Preview,
+                            ReplacementStringItem.Release);
+                    }
+                }
+
+                FileUtilities.SaveTextToFileAsUtf8Bom(FilePath, Content);
             }
 
-            FileUtilities.SaveTextToFileAsUtf8Bom(FilePath, Content);
+            {
+                List<(string Release, string Preview)> ReplacementStringList =
+                new List<(string Release, string Preview)>();
+                ReplacementStringList.Add((
+                    "<Content Include=\"..\\Assets\\PackageAssets\\**\\*\">",
+                    "<Content Include=\"..\\Assets\\PreviewPackageAssets\\**\\*\">"));
+
+                string FilePath = string.Format(
+                    @"{0}\NanaGetPackage\NanaGetPackage.wapproj",
+                    ProjectRootPath);
+
+                string Content = File.ReadAllText(
+                    FilePath,
+                    Encoding.UTF8);
+                foreach (var ReplacementStringItem in ReplacementStringList)
+                {
+                    if (Preview)
+                    {
+                        Content = Content.Replace(
+                            ReplacementStringItem.Release,
+                            ReplacementStringItem.Preview);
+                    }
+                    else
+                    {
+                        Content = Content.Replace(
+                            ReplacementStringItem.Preview,
+                            ReplacementStringItem.Release);
+                    }
+                }
+
+                FileUtilities.SaveTextToFileAsUtf8Bom(FilePath, Content);
+            }
 
             Console.WriteLine("Hello, World!");
             Console.ReadKey();
