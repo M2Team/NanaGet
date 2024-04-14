4350e4e8e002fdc5d2609f9e7f13a48620648f33
MouriNaruto <Kenji.Mouri@outlook.com>
2024-04-14 20:53:13 +0800

    Create NanaGet.Aria2..

---
+++ NanaGet/NanaGet.Aria2.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   NanaGet
+ * FILE:      NanaGet.Aria2.cpp
+ * PURPOSE:   Implementation for the NanaGet.Aria2
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "NanaGet.Aria2.h"
---
+++ NanaGet/NanaGet.Aria2.h
@@ -0,0 +1,27 @@
+﻿/*
+ * PROJECT:   NanaGet
+ * FILE:      NanaGet.Aria2.h
+ * PURPOSE:   Definition for the NanaGet.Aria2
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef NANAGET_ARIA2
+#define NANAGET_ARIA2
+
+#if (defined(__cplusplus) && __cplusplus >= 201703L)
+#elif (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L)
+#else
+#error "[NanaGetCore] You should use a C++ compiler with the C++17 standard."
+#endif
+
+#include <cstdint>
+
+namespace NanaGet::Aria2
+{
+
+}
+
+#endif // !NANAGET_ARIA2
