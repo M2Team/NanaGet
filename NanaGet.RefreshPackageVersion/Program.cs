001fc3049d0fae2fd7696f26252c8f6d2cd21982
MouriNaruto <Kenji.Mouri@outlook.com>
2024-06-22 14:45:28 +0800

    Create NanaGet.RefreshPackageVersion project.

---
+++ NanaGet.RefreshPackageVersion/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NanaGet.RefreshPackageVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "NanaGet.RefreshPackageVersion task has been completed.");
+            Console.ReadKey();
+        }
+    }
+}
