31fc8921231c57f83a6c2c94fefdc5e38e383326
MouriNaruto <Kenji.Mouri@outlook.com>
2024-06-22 14:37:49 +0800

    Create NanaGet.ProjectAssetsGenerator project.

---
+++ NanaGet.ProjectAssetsGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NanaGet.ProjectAssetsGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "NanaGet.ProjectAssetsGenerator task has been completed.");
+            Console.ReadKey();
+        }
+    }
+}
